34bf279ef82bcf07865a10c13c8b4a4a2fcbfa25
Tarpantone <baptistekinif@hotmail.com>
2023-05-11 17:34:59 +0200

    Modif Javadoc Interface

--- com/hemebiotech/analytics/ISymptomReader.java
+++ com/hemebiotech/analytics/ISymptomReader.java
@@ -1,7 +1,20 @@
 package com.hemebiotech.analytics;
 
 import java.util.Map;
+/*
+ * Anything that will read symptom data from a source
+ * The important part is, the return value from the operation, which is a map of Strings key and Integers value,
+ * value that will be equal to the number of occurrences of the associated key in the source.
+ *
+ * The implementation does not need to order the Map.
+ *
+ */
 public interface ISymptomReader {
-
+    /*
+     * If no data is available, return an empty Map.
+     *
+     * @return a map of Strings key and Integers value,
+     * value that will be equal to the number of occurrences of the associated key in the source.
+     */
     Map<String,Integer> getSymptoms ();
 }
--- com/hemebiotech/analytics/ISymptomWriter.java
+++ com/hemebiotech/analytics/ISymptomWriter.java
@@ -1,7 +1,19 @@
 package com.hemebiotech.analytics;
 
 import java.util.Map;
-
+/*
+ * Anything that will write symptom data to a source
+ * The important part is, the symptoms are listed in a map, which is a map of Strings key and Integers value,
+ * value that will be equal to the number of occurrences of the associated key in the source.
+ *
+ * The implementation does not need to order the Map.
+ */
 public interface ISymptomWriter {
+    /*
+     * If no data is available,close the writer and exit.
+     *
+     * @param a map of Strings key and Integers value,
+     * value that will be equal to the number of occurrences of the associated key in the source.
+     */
     public void writeSymptoms(Map<String, Integer> symptoms);
 }
